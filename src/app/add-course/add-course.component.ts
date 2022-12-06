df8c91553424857acc9613f1da420f37e2fc5fe6
delmashajan <delmashajan@gmail.com>
2022-12-06 19:56:53 +0530

    services added

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -7,15 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AddCourseComponent {
 
+
+
   courseTitle=""
   courseDescription=""
   courseVenue=""
   duration=""
   courseDate=""
 
+  constructor(private api:ApiService){}
+
   readValue =()=>{
     let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseVenue":this.courseVenue,
     "duration":this.duration,"courseDate":this.courseDate}
+    console.log(data)
+    this.api.addCourse(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if(response.status=="success"){
+          alert("added successfully")
+          this.courseTitle=""
+          this.courseDescription=""
+          this.courseVenue=""
+          this.duration=""
+          this.courseDate=""
+        }else{
+          alert("something went wrong")
+        }
+      }
+    )
   }
 
 }
